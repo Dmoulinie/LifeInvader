d64a6c42aba0c7e1118ea7acf91b653680e95d9c
Dan Moulinie <dan.moulinie@gmail.com>
2024-04-30 15:40:30 +1100

    DockerFile Javascript

--- back/commentaires/app.js
+++ back/commentaires/app.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
+const PORT = 3000;
 const HOST = '0.0.0.0'
 
+
+// MongoDB
+
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://127.0.0.1:27017/commentaires')
+//   .then(() => console.log('Connected!'));
+
 // App
-app.get('/', (req, res) => {
-  res.send('Hello World')
+app.get('/', async(req, res)=>{
+  res.status(200).json({message : 'Hello world'})
+})
+
+app.get("/db", (req, res) => {
+  
+  res.send("Hello World from db");
 });
 
 app.listen(PORT, HOST)
+
+
+
 console.log(`Our app running on http://${HOST}:${PORT}`)
