9469e9d4daad9eb658ca521714fca2dd0f36e4e9
Dan Moulinie <dan.moulinie@gmail.com>
2024-05-09 00:37:13 +1100

    Fix: folder name + update class

--- back/connexion/social/src/main/java/com/app/oauth2/social/SocialApplication.java
+++ back/connexion/social/src/main/java/com/app/oauth2/social/SocialApplication.java
@@ -1,32 +1,15 @@
 package com.app.oauth2.social;
 
-import com.app.oauth2.social.models.User;
-import com.app.oauth2.social.repositories.UserRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SocialApplication implements CommandLineRunner {
+public class SocialApplication {
 
-	@Autowired
-    private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialApplication.class, args);
 	}
 
-	@Override
-    public void run(String... args) throws Exception {
-        // fetch all Albums
-        System.out.println("Albums found with findAll():");
-        System.out.println("-------------------------------");
-        for (User Album : userRepository.findAll()) {
-            System.out.println(Album.getName());
-        }
-        System.out.println();
-    }
 
 }
