0e4625da46f042c89575aebac41971d598672d74
Kearan2002 <pechkearan2002@gmail.com>
2024-05-08 11:15:54 +1100

    small-changes

---
+++ front/react/src/components/pages/user/userpage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const User = () => {
+    return (
+        <div className='pt-[3.25rem] flex flex-row 2xl:w-[70rem] xl:w-[63rem] lg:w-[45rem] w-full border border-black mx-auto'>
+            PAGE D'UTILISATEUR
+        </div>
+    );
+};
+
+export default User;
